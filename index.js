9235da9bc6d846d0ef2101d6e5138dc54c77f2b4
jamesndungu <jimmindungu3@gmail.com>
2023-09-25 14:21:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+const superbowlResults = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+function superbowlWin(superbowlResults) {
+    const winningYearObject = superbowlResults.find(resultObject => resultObject.result === "W");
+  
+    if (winningYearObject) {
+      return winningYearObject.year;
+    } else {
+      return undefined;
+    }
+  }
+  
